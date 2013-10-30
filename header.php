60d0563caad28aa3dcd29236cc47aa0e38e8cf69
Al Nemec <al.nemec@gmail.com>
2013-10-29 21:32:00 -0500

    Resolving a Bug with the header image.

--- header.php
+++ header.php
@@ -147,12 +147,10 @@ $current_colorscheme = $options['link_color'];
 				<?php
 					// The header image
 					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
-							$image[1] >= $header_image_width ) :
+					
 						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else :
+						//echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+					
 						// Compatibility with versions of WordPress prior to 3.4.
 						if ( function_exists( 'get_custom_header' ) ) {
 							$header_image_width  = get_custom_header()->width;
@@ -163,7 +161,7 @@ $current_colorscheme = $options['link_color'];
 						}
 						?>
 					<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
+				
 			</a>
 			<?php endif; // end check for removed header image ?>
 
