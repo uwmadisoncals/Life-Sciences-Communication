658e6c307de475dc5e2f6f2ca6922b7b657e7936
Daniel Dropik <ddropik@wisc.edu>
2016-03-28 12:58:28 -0500

    Update Dontation link in footer

--- footer.php
+++ footer.php
@@ -20,21 +20,21 @@
 						/* A sidebar in the footer? Yep. You can can customize
 						 * your footer with four columns of widgets.
 						 */
-						
+
 							get_sidebar( 'footer' );
 					?>            <div class="clearfix"></div>
 			</div>
 			<div class="linksContainer right">
 				<h3>Support</h3>
-				<p>You can help support Life Sciences 
-Communication by making a gift to the 
+				<p>You can help support Life Sciences
+Communication by making a gift to the
 University of Wisconsin Foundation.</p>
-				<a href="https://secure.supportuw.org/MultiPage/processStep1.do?seq=1441" class="button">Make a Gift</a>
+				<a href="https://www.myuwconnect.org/give?id=984FE62C-0739-438F-B9D9-A97E2FFFD61B" class="button">Make a Gift</a>
 				<p class="address">Department of Life Sciences Communication | Hiram Smith Hall | 1545 Observatory Drive | Madison, WI 53706 | 608.262.1464 </p>
 			</div>
 			<div class="clearfix"></div>
         </div>
-        
+
         <div class="relative">
         <div class="copyright">
         	<img src="<?php echo get_template_directory_uri(); ?>/images/footercrest.png" alt="University of Wisconsin Madison" />
@@ -46,13 +46,13 @@ University of Wisconsin Foundation.</p>
 				/* A sidebar in the footer? Yep. You can can customize
 				 * your footer with four columns of widgets.
 				 */
-				
+
 					get_sidebar( 'footer' );
 			?>
 		</div>
 		<div class="socialRef">
 			<a href="https://twitter.com/uw_lsc" class="twitter">Twitter</a>
-		
+
 			<a href="https://www.facebook.com/UWMadisonLSC" class="facebook">Facebook</a>
 		</div>
 			<div class="clearfix"></div>
@@ -69,6 +69,6 @@ University of Wisconsin Foundation.</p>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-rotate.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/browserdetect.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/mainactions.js"></script>
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/twitterfetcher_v15.js"></script> 
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/twitterfetcher_v15.js"></script>
 </body>
 </html>
